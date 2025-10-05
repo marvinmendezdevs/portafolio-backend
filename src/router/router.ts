4263c41fe63c64cbe5a2a1ee4302e809f8126135
Marvin Mendez <marvinmendezdevs@gmail.com>
2025-10-05 16:49:11 -0600

    Adding new URI to get User Info

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,6 +5,15 @@ import User from "../models/User";
 const router = Router();
 
 // Personal information routin
+router.get('/personal-information', async (req, res) => {
+    try {
+        const data = await User.findOne();
+        res.status(201).json({ status: "ok", message: "Personal information loaded.", data});
+    } catch (error) {
+        res.status(404).json({ status: "failed", message: "Personal information not found"});
+    }
+})
+
 router.post('/personal-information', async (req, res) => {
     const { body } = req;
 
