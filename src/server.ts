b6c0bc0c2fc00d6d63f34557c231e2d6c5900f76
Marvin Méndez <marvinmendezdevs@gmail.com>
2025-10-06 12:47:11 -0600

    Enable Cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/db';
 import router from './router/router';
@@ -8,6 +9,11 @@ const app = express();
 
 // Enable JSON Data
 app.use(express.json());
+app.use(cors({
+  origin: '*',                  // todos los dominios
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Start DB connection
 connectDB();
